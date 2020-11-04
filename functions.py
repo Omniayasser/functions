f983ceb10387d70812e6c8f6934cc99a31fdb098
Omniayasser <73958351+Omniayasser@users.noreply.github.com>
2020-11-04 21:40:23 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,19 +1,22 @@
+#function 1 to show the list
 def show_menu():
     print(" (1) Check prime")
     print(" (2) Check palindrome")
     print(" (3) Exit")
 
-
+#function 2, prime number checker
 def is_prime(num):
     if num > 1:
         for x in range (2, num):
-            if (num % x)== 0:
+            if (num % x)== 0:   #if the num is divisable by any number in a range(2, num -1) is not a prime
                 print(num,"is not a prime number")
                 break
         else:
                 print(num, "is a prime number")
     else:
         print(num, "is not a prime number")
+
+ # function 3, check if the input is plaindrome or not
 def is_plaindrome(word):
     Reversed_Word= word[::-1]
     if word== Reversed_Word:
@@ -22,7 +25,7 @@ def is_plaindrome(word):
         print(word," is not a palindrome")
 
 count = 1
-while count == 1:
+while count == 1: 
     show_menu()
     opperation= int(input(" Choose an opperation"))
     if opperation == 1:
@@ -33,6 +36,6 @@ while count == 1:
         is_plaindrome(word)
     elif opperation == 3:
         print(" thank you :)")
-        count+=1
+        count+=1 #change the condition to stop while loop and exit the a program
     else:
         print(" invalid input, try again. ")
